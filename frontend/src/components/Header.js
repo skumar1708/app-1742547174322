24e8fe94bccee4cb8d9ab17763fb3fa49b6696b7
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-21 14:24:43 +0530

    Automated commit: frontend/src/components/Header.js

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Welcome to Lord Shiva Image Gallery</h1>
+    </header>
+  );
+}
+
+export default Header;
