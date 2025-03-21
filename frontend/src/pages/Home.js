b3fceb2ffaa564672f1c3b67c27f65f951d5654b
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-21 14:25:07 +0530

    Automated commit: frontend/src/pages/Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageGallery from '../components/ImageGallery';
+
+function Home() {
+  return (
+    <div>
+      <h2>Explore the Gallery</h2>
+      <ImageGallery />
+    </div>
+  );
+}
+
+export default Home;
