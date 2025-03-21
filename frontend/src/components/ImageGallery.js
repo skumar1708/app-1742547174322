df5f1e125ff0e0e65a28002ee3aa24c5d527ce04
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-21 14:24:54 +0530

    Automated commit: frontend/src/components/ImageGallery.js

---
+++ frontend/src/components/ImageGallery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ImageGallery() {
+  return (
+    <div className="gallery">
+      <img src="/images/lord-shiva.jpg" alt="Lord Shiva" />
+    </div>
+  );
+}
+
+export default ImageGallery;
